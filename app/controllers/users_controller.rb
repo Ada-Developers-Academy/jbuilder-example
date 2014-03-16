9d100bde0133a0ab67346f54ea4c69cb9775ef26
Bookis Smuin <bookis.smuin@gmail.com>
2014-03-16 10:54:30 -0700

    adds create action and specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(params.require(:user).permit(:username, :email))
+    if @user.save
+      render "show"
+    else
+      
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,4 +21,17 @@ describe UsersController do
       expect(response.body).to eq(attrs.to_json)
     end
   end
+  
+  describe "POST 'create'" do 
+    let(:request) { post :create, user: attrs, format: :json }
+    it "creates a user" do
+      expect { request }.to change(User, :count).by(1)
+    end
+    
+    it "returns json" do
+      request
+      attrs.merge!(id: assigns(:user).id)
+      expect(response.body).to eq(attrs.to_json)
+    end
+  end
 end
