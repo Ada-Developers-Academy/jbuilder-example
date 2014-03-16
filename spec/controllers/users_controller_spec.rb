7dba57a4b7008159d0aef5ddbdfcb90e1a5c871b
Bookis Smuin <bookis.smuin@gmail.com>
2014-03-16 10:48:21 -0700

    adds user show json and specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 
 describe UsersController do
   render_views
+  let(:attrs) { {id: 1, email: "a@b.com", username: "b"} }
+  
   describe "GET 'index'" do
-    let(:attrs) { {id: 1, email: "a@b.com", username: "b"} }
     let!(:user) { User.create(attrs)}
+    
     it "returns json" do 
       get :index, format: :json
       expect(response.body).to eq({results: [attrs]}.to_json)
+    end
+  end
+  
+  describe "GET 'show'" do
+    let!(:user) { User.create(attrs)}
+    
+    it "returns json" do
+      get :show, id: user.id, format: :json
+      expect(response.body).to eq(attrs.to_json)
     end
   end
 end
